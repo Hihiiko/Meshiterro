4eb11b734585ae65c2a777ba337d2c47f28a9b1c
Hihiiko <211629695+Hihiiko@users.noreply.github.com>
2025-06-19 11:06:22 +0000

    [finish]4-24

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller? 
   #device利用機能（ユーザ登録、ログイン認証など）が使われる前に
   #configure_permitted_parametersメソッドが実行されます。↓
   
   def after_sign_in_path_for(resource) #after_sign_in_path_forはDeviseが用意しているメソッドで、サインイン後にどこに遷移するかを設定しているメソッドです
-    about_path                          #after_sign_in_path_forは、Deviseの初期設定ではroot_pathになっています。サインイン後にルートパスに遷移していたのはこのためです。
+    post_images_path                         #after_sign_in_path_forは、Deviseの初期設定ではroot_pathになっています。サインイン後にルートパスに遷移していたのはこのためです。
                                          # 上記のような記述をすることで、初期設定を上書きすることができます。
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
    
   has_many :post_images, dependent: :destroy #userのあるデータを削除すると、その関係のあるarticlesを自動的にすべて削除できる。
                       #deoendent: destroyの性質により、user.idをキーとして持ってるすべてのarticleをすべて消すことができる。
+  has_many :post_comments, dependent: :destroy  
+  has_one_attached :profile_image
+  has_many :favorites, dependent: :destroy
+
+  
+  def get_profile_image(width, height)
+  unless profile_image.attached?
+   file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+   profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
